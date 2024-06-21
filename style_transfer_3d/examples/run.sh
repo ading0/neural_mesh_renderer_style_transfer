ac5ac83f1d44da33d6439dd088a66663af2d19ca
ading0 <143818271+ading0@users.noreply.github.com>
2024-06-21 06:00:38 -0400

    fixed texture mapping; bugfixes

--- style_transfer_3d/examples/run.sh
+++ style_transfer_3d/examples/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 DIR=./examples/data
-python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/gris1.jpg -o ${DIR}/results/teapot_gris.gif -lc 2000000000 -ltv 10000
-python ./examples/run.py -im ${DIR}/meshes/bunny.obj -is ${DIR}/styles/munch1.jpg -o ${DIR}/results/bunny_munch.gif -lc 2000000000 -ltv 100000
+# python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/coupland1.jpg -o ${DIR}/results/teapot_coupland1.gif -rd ${DIR}/results
+python ./examples/run.py -im ${DIR}/meshes/bunny.obj -is ${DIR}/styles/gogh2.jpg -o ${DIR}/results/bunny_gogh2.gif -rd ${DIR}/results
+python ./examples/run.py -im ${DIR}/meshes/bunny.obj -is ${DIR}/styles/klee1.jpg -o ${DIR}/results/bunny_klee1.gif -rd ${DIR}/results
+# python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/lettl1.jpg -o ${DIR}/results/teapot_lettl1.gif -rd ${DIR}/results
+# python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/sketch1.jpg -o ${DIR}/results/teapot_sketch1.gif -rd ${DIR}/results
+# python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/monet4.jpg -o ${DIR}/results/teapot_monet4.gif -rd ${DIR}/results
